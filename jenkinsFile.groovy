afaaaaf5d4dbef6d468acbb04bdc9c1e8e0ebf2c
sunnyangeleena <sunnyangeleena22@gmail.com>
2022-11-25 12:31:34 +0530

    Added ECR files

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -1,11 +1,18 @@
 pipeline {
 	agent any
 	stages {
-		stage('Java Compile') {
+		stage('AWS login') {
 			steps {
-				echo "HI ANGELENNA"
+				sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 491342662748.dkr.ecr.us-east-1.amazonaws.com"
+			}
+		}
+		stage('AWS ECR push') {
+			steps {
+				sh "docker push 491342662748.dkr.ecr.us-east-1.amazonaws.com/devops:jenkins"
 			}
 		}
         	
 	}
+
+
 }
